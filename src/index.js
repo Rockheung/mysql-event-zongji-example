4298339683ef45fda169210e8f59176773e37a70
Rockheung <rockheung@gmail.com>
2019-12-29 05:50:39 +0900

    hello

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // Client code
 const ZongJi = require("zongji");
+const { Parser } = require("node-sql-parser");
 
 const { HOSTNAME: host, USERNAME: user, PASSWORD: password } = process.env;
 
@@ -11,15 +12,35 @@ const zongji = new ZongJi({
 });
 
 zongji.on("binlog", function(evt) {
-  evt.dump();
+  try {
+    if (evt.getEventName() === "query" && evt.query.length) {
+      const parser = new Parser();
+      console.log(evt.query);
+      const { tableList, columnList, ast } = parser.parse(evt.query);
+      console.log(ast);
+    }
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      console.error(e);
+      return;
+    }
+    console.log(e);
+  }
 });
 
 zongji.start({
-  includeEvents: ["tablemap", "writerows", "updaterows", "deleterows"]
+  includeEvents: [
+    "tablemap",
+    "writerows",
+    "updaterows",
+    "deleterows",
+    "query",
+    "xid"
+  ]
 });
 
 process.on("SIGINT", function() {
-  console.log("Got SIGINT.");
+  // console.log("Got SIGINT.");
   zongji.stop();
   process.exit();
 });
